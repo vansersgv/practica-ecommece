8bd5e16625d3f11f38cb19dc9568d1d5cdeb209a
sergio vergara <vansersgv@gmail.com>
2023-11-30 23:39:45 -0300

    docuApi

--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -3,38 +3,46 @@ import cartsController from '../controllers/carts.controller.js';
 import { authorization, passportError } from '../utils/messageErrors.js';
 const routerCart = Router();
 
-routerCart.get('/', cartsController.getCarts);
-routerCart.get('/:cid', cartsController.getCart);
+routerCart.get('/', cartsController.getCarts); //
+routerCart.get('/:cid', cartsController.getCart); //
 routerCart.post(
 	'/:cid/purchase',
 	passportError('jwt'),
-	authorization('user'),
+	authorization(['user', 'premium']),
 	cartsController.purchaseCart
 );
-routerCart.post('/', cartsController.postCart);
+routerCart.post('/', cartsController.postCart); //
 routerCart.put(
 	'/:cid/product/:pid',
 	passportError('jwt'),
-	authorization('user'),
+	authorization(['user', 'premium']),
 	cartsController.putProductToCart
 );
 routerCart.put(
-	'/:cid/products/:pid',
+	'/:cid/products/:pid', //
 	passportError('jwt'),
-	authorization('user'),
+	authorization(['user', 'premium']),
 	cartsController.putQuantity
 );
 routerCart.put(
+	//
 	'/:cid',
 	passportError('jwt'),
-	authorization('user'),
+	authorization(['user', 'premium']),
 	cartsController.putProductsToCart
 );
-routerCart.delete('/:cid', passportError('jwt'), authorization('user'), cartsController.deleteCart);
 routerCart.delete(
+	//
+	'/:cid',
+	passportError('jwt'),
+	authorization(['user', 'premium']),
+	cartsController.deleteCart
+);
+routerCart.delete(
+	//
 	'/:cid/products/:pid',
 	passportError('jwt'),
-	authorization('user'),
+	authorization(['user', 'premium']),
 	cartsController.deleteProductFromCart
 );
 export default routerCart;
